573d9e0aa2dc02d38c1322dd9b8893fff82377e2
chunhui-pang <chunhui.pang@outlook.com>
2018-05-27 02:51:49 +0800

    update to config

--- config/02-language-specific/term-mode.el
+++ config/02-language-specific/term-mode.el
@@ -2,7 +2,8 @@
 (defun customize-for-term()
   (message "customize for term/ansi-term mode...")
   (linum-mode 0)
-  (add-to-list 'company-backends 'company-dict))
+  (add-to-list 'company-backends 'company-dict)
+  (local-unset-key (kbd "C-c C-r")))
 
 
 (add-hook 'term-mode-hook 'customize-for-term)
