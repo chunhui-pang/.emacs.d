9de40a797aeddd7822ea4c94742e189adbb505ba
pangchunhui <pangchunhui@bytedance.com>
2025-11-13 00:05:20 +0800

    feat: fix rime panic

--- config/00-package-specific/rime.el
+++ config/00-package-specific/rime.el
@@ -1,5 +1,8 @@
+;; rime should be compiled in manual, see: https://github.com/rime/librime/blob/master/README-mac.md
+;; after compile, the following commands should be executed in build directory
+;; mkdir include; cp ../src/rime_api.h include/
 (use-package rime
   :custom
   (default-input-method "rime")
-  (rime-librime-root "/opt/homebrew")
+  (rime-librime-root "~/.emacs.d/librime/build/")
   (rime-show-candidate 'posframe))
